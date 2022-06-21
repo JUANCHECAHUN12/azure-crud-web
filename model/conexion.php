2215e2070f75976df9fb021a7e23e18dc1450375
JUANCHECAHUN12 <a.juan.che@utponiente.edu.mx>
2022-06-21 11:16:57 -0500

    first

---
+++ model/conexion.php
@@ -0,0 +1,17 @@
+<?php 
+$contrasena = "Daniel141516";
+$usuario = "root_admin";
+$nombre_bd = "crud";
+
+try {
+	$bd = new PDO (
+		'mysql:host=utpservidor123456789.mysql.database.azure.com;
+		dbname='.$nombre_bd,
+		$usuario,
+		$contrasena,
+		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+	);
+} catch (Exception $e) {
+	echo "Problema con la conexion: ".$e->getMessage();
+}
+?>
